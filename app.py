916b368eba135b6ba876c86ab6b59e6637f39487
Akhlaque Alam <akhlaquealam5142@gmail.com>
2026-07-15 16:01:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,118 +114,118 @@ st.caption(
 )
 
 # Model Comparison Before Hyperparameter Tuning
-comparison_df = pd.DataFrame({
-    "Model": [
-        "CatBoost",
-        "XGBoost",
-        "LightGBM",
-        "Random Forest",
-        "Decision Tree",
-        "Gradient Boosting",
-        "KNN Regressor",
-        "Linear Regression",
-        "SVM Regressor"
-    ],
-    "MAE": [
-        23.8946,
-        24.1474,
-        25.3265,
-        24.5053,
-        34.1010,
-        45.2310,
-        53.2738,
-        86.1662,
-        76.9182
-    ],
-    "MSE": [
-        1473.8083,
-        1543.5446,
-        1634.4686,
-        1694.6802,
-        3373.8022,
-        4344.1558,
-        6164.7317,
-        13110.2863,
-        14078.6922
-    ],
-    "RMSE": [
-        38.3902,
-        39.2880,
-        40.4286,
-        41.1665,
-        58.0844,
-        65.9102,
-        78.5158,
-        114.5002,
-        118.6537
-    ],
-    "R2_Score": [
-        0.9535,
-        0.9513,
-        0.9484,
-        0.9465,
-        0.8935,
-        0.8628,
-        0.8053,
-        0.5860,
-        0.5554
-    ]
-})
+# comparison_df = pd.DataFrame({
+#     "Model": [
+#         "CatBoost",
+#         "XGBoost",
+#         "LightGBM",
+#         "Random Forest",
+#         "Decision Tree",
+#         "Gradient Boosting",
+#         "KNN Regressor",
+#         "Linear Regression",
+#         "SVM Regressor"
+#     ],
+#     "MAE": [
+#         23.8946,
+#         24.1474,
+#         25.3265,
+#         24.5053,
+#         34.1010,
+#         45.2310,
+#         53.2738,
+#         86.1662,
+#         76.9182
+#     ],
+#     "MSE": [
+#         1473.8083,
+#         1543.5446,
+#         1634.4686,
+#         1694.6802,
+#         3373.8022,
+#         4344.1558,
+#         6164.7317,
+#         13110.2863,
+#         14078.6922
+#     ],
+#     "RMSE": [
+#         38.3902,
+#         39.2880,
+#         40.4286,
+#         41.1665,
+#         58.0844,
+#         65.9102,
+#         78.5158,
+#         114.5002,
+#         118.6537
+#     ],
+#     "R2_Score": [
+#         0.9535,
+#         0.9513,
+#         0.9484,
+#         0.9465,
+#         0.8935,
+#         0.8628,
+#         0.8053,
+#         0.5860,
+#         0.5554
+#     ]
+# })
 
 
-# Model Performance After Hyperparameter Tuning
-tuning_comparison_df = pd.DataFrame({
-    "Model": [
-        "CatBoost (Tuned)",
-        "XGBoost (Tuned)",
-        "LightGBM (Tuned)",
-        "CatBoost (Original)",
-        "XGBoost (Original)",
-        "LightGBM (Original)",
-        "Random Forest (Tuned)",
-        "Random Forest (Original)"
-    ],
-    "MAE": [
-        21.9912,
-        22.5425,
-        22.5871,
-        23.8946,
-        24.1474,
-        25.3265,
-        24.4813,
-        24.5053
-    ],
-    "MSE": [
-        1280.1439,
-        1378.5630,
-        1381.9522,
-        1473.8083,
-        1543.5446,
-        1634.4686,
-        1689.1659,
-        1694.6802
-    ],
-    "RMSE": [
-        35.7791,
-        37.1290,
-        37.1746,
-        38.3902,
-        39.2880,
-        40.4286,
-        41.0995,
-        41.1665
-    ],
-    "R2_Score": [
-        0.9596,
-        0.9565,
-        0.9564,
-        0.9535,
-        0.9513,
-        0.9484,
-        0.9467,
-        0.9465
-    ]
-})
+# # Model Performance After Hyperparameter Tuning
+# tuning_comparison_df = pd.DataFrame({
+#     "Model": [
+#         "CatBoost (Tuned)",
+#         "XGBoost (Tuned)",
+#         "LightGBM (Tuned)",
+#         "CatBoost (Original)",
+#         "XGBoost (Original)",
+#         "LightGBM (Original)",
+#         "Random Forest (Tuned)",
+#         "Random Forest (Original)"
+#     ],
+#     "MAE": [
+#         21.9912,
+#         22.5425,
+#         22.5871,
+#         23.8946,
+#         24.1474,
+#         25.3265,
+#         24.4813,
+#         24.5053
+#     ],
+#     "MSE": [
+#         1280.1439,
+#         1378.5630,
+#         1381.9522,
+#         1473.8083,
+#         1543.5446,
+#         1634.4686,
+#         1689.1659,
+#         1694.6802
+#     ],
+#     "RMSE": [
+#         35.7791,
+#         37.1290,
+#         37.1746,
+#         38.3902,
+#         39.2880,
+#         40.4286,
+#         41.0995,
+#         41.1665
+#     ],
+#     "R2_Score": [
+#         0.9596,
+#         0.9565,
+#         0.9564,
+#         0.9535,
+#         0.9513,
+#         0.9484,
+#         0.9467,
+#         0.9465
+#     ]
+# })
 
 
 
@@ -354,63 +354,63 @@ st.divider()
 
 
 # Model Performance Before Hyperparameter Tuning
-st.subheader("Baseline Model Comparison")
+# st.subheader("Baseline Model Comparison")
 
-st.dataframe(
-    comparison_df.sort_values(
-        by='R2_Score',
-        ascending=False
-    ),
-    use_container_width=True
-)
+# st.dataframe(
+#     comparison_df.sort_values(
+#         by='R2_Score',
+#         ascending=False
+#     ),
+#     use_container_width=True
+# )
 
-# Model Performance After Hyperparameter Tuning
-st.subheader("Model Performance After Hyperparameter Tuning")
+# # Model Performance After Hyperparameter Tuning
+# st.subheader("Model Performance After Hyperparameter Tuning")
 
-st.dataframe(
-    tuning_comparison_df.sort_values(
-        by='R2_Score',
-        ascending=False
-    ),
-    use_container_width=True
-)
+# st.dataframe(
+#     tuning_comparison_df.sort_values(
+#         by='R2_Score',
+#         ascending=False
+#     ),
+#     use_container_width=True
+# )
 
 # SELECTED Bike Rental SCENARIO
-st.subheader("Selected Bike Rental Scenario")
-scenario_df = pd.DataFrame({
-    "Feature": [
-        "Hours",
-        "Month",
-        "Week Day",
-        "Temperature",
-        "Feels Like Temp",
-        "Humidity",
-        "Wind Speed",
-        "Season",
-        "Year",
-        "Holiday",
-        "Working Day",
-        "Weather",
-        "Rush Hour"
-    ],
-    "Value": [
-        hr,
-        mnth,
-        weekday,
-        temp,
-        atemp,
-        hum,
-        windspeed,
-        season,
-        yr,
-        holiday,
-        workingday,
-        weathersit,
-        rush_hour
-    ]
-})
+# st.subheader("Selected Bike Rental Scenario")
+# scenario_df = pd.DataFrame({
+#     "Feature": [
+#         "Hours",
+#         "Month",
+#         "Week Day",
+#         "Temperature",
+#         "Feels Like Temp",
+#         "Humidity",
+#         "Wind Speed",
+#         "Season",
+#         "Year",
+#         "Holiday",
+#         "Working Day",
+#         "Weather",
+#         "Rush Hour"
+#     ],
+#     "Value": [
+#         hr,
+#         mnth,
+#         weekday,
+#         temp,
+#         atemp,
+#         hum,
+#         windspeed,
+#         season,
+#         yr,
+#         holiday,
+#         workingday,
+#         weathersit,
+#         rush_hour
+#     ]
+# })
 
-st.dataframe(
-    scenario_df.astype(str),
-    use_container_width=True
-)
+# st.dataframe(
+#     scenario_df.astype(str),
+#     use_container_width=True
+# )
