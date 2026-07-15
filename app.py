7dfff48dac58f04d9a352834eab10864c1da7d3b
Akhlaque Alam <akhlaquealam5142@gmail.com>
2026-07-15 12:13:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ st.set_page_config(
 # Load Model
 model = joblib.load("catboost_final_model.pkl")
 feature_columns = joblib.load("feature_columns.pkl")
+try:
+    model.set_params(thread_count=1)
+except:
+    pass
 
 
 
